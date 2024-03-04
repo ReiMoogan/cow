74ead2569e3397b3f550ea2d413dfd3638f0bf30
DoggySazHi <doggysazhi@gmail.com>
2024-03-03 22:11:27 -0800

    hotfix moogan

--- src/commands/general/moogan.rs
+++ src/commands/general/moogan.rs
@@ -10,6 +10,7 @@ use serenity::model::channel::AttachmentType;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct DallERequest {
+    pub model: String,
     pub prompt: String,
     pub n: u8,
     pub size: String
@@ -35,27 +36,31 @@ pub struct DallEImage {
 pub async fn moogan(ctx: CowContext<'_>) -> Result<(), Error> {
     ctx.defer().await?;
 
-    let client = Client::builder().danger_accept_invalid_certs(true).timeout(Duration::from_secs(10)).build().unwrap();
-    const URL: &str = "https://reimu.williamle.com";
-    if let Ok(response) = client.get(URL).send().await {
-        if response.status().is_success() {
-            let bytes = response.bytes().await?;
-
-            ctx.send(|m| m.embed(|e|
-                e
-                    .title("Live Moogan Reaction")
-                    .attachment("moogan_live_reaction.png")
-            ).attachment(AttachmentType::Bytes { data: Cow::from(bytes.as_ref()), filename: "moogan_live_reaction.png".to_string() })).await?;
-            return Ok(());
-        }
-    }
+    // reimu.williamle.com is down :(
+    // let client = Client::builder().danger_accept_invalid_certs(true).timeout(Duration::from_secs(10)).build().unwrap();
+    // const URL: &str = "https://reimu.williamle.com";
+    // if let Ok(response) = client.get(URL).send().await {
+    //     if response.status().is_success() {
+    //         let bytes = response.bytes().await?;
+    //
+    //         ctx.send(|m| m.embed(|e|
+    //             e
+    //                 .title("Live Moogan Reaction")
+    //                 .attachment("moogan_live_reaction.png")
+    //         ).attachment(AttachmentType::Bytes { data: Cow::from(bytes.as_ref()), filename: "moogan_live_reaction.png".to_string() })).await?;
+    //         return Ok(());
+    //     }
+    // }
 
     let config_json = fs::read_to_string("config.json").await?;
     let config : Config = serde_json::from_str(&config_json).expect("config.json is malformed");
 
-    const MOOGAN_PROMPT: &str = "a photo of Reimu Hakurei from Touhou Project in a cow onesie looking at the viewer, anime, cartoon, no human characteristics, high quality, adult";
+    // const MOOGAN_PROMPT: &str = "a solo no-text photo of Reimu Hakurei from Touhou Project in a cow onesie looking at the viewer, professional anime, anime style, cartoon, brown hair, Hakurei Reimu cosplay, shrine maiden, red hair tubes, no human characteristics, high quality, high quality shading, soft coloring";
+    // Revised by GPT (forced)
+    const MOOGAN_PROMPT: &str = "I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS: A professional, high-quality anime style depiction of Reimu Hakurei dressed in a cow onesie. The character gazes at the viewer with entrancing, expressive eyes, adding to the charm of her outfit. This character maintains an anime look but bears no discernable human characteristics. Intricate shading and soft coloring techniques add depth and two-dimensionality, enhancing the overall aesthetic of the image.";
 
     let body = DallERequest {
+        model: "dall-e-3".to_string(),
         prompt: MOOGAN_PROMPT.to_string(),
         n: 1,
         size: "1024x1024".to_string()
@@ -71,6 +76,28 @@ pub async fn moogan(ctx: CowContext<'_>) -> Result<(), Error> {
         .bearer_auth(config.openai_api_key)
         .body(body_serialized)
         .send().await.map(|r| r.json::<DallEResponse>());
+    //     .send().await.map(|r| r.text());
+    //
+    // let response = match response {
+    //     Ok(r) => r.await,
+    //     Err(ex) => {
+    //         error!("Failed to generate image: {}", ex);
+    //         ctx.say("I couldn't generate an image...").await?;
+    //         return Ok(());
+    //     }
+    // };
+    //
+    // let response = match response {
+    //     Ok(r) => r,
+    //     Err(ex) => {
+    //         error!("Failed to parse JSON: {}", ex);
+    //         ctx.say("I couldn't generate an image...").await?;
+    //         return Ok(());
+    //     }
+    // };
+    //
+    // error!("response: {}", response);
+    // ctx.say(format!("```json\n{}\n```", response)).await?;
 
     let response = match response {
         Ok(r) => r.await,
